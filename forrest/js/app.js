0e597269b820f4a989e74cd73e0c8c0821fb5d3a
Forrest Wilson <forrest.wilson@me.com>
2018-03-10 00:03:09 +1300

    Added a getDesigners method that talks to the API, fetches data and compiles a template to be appended to the page

--- forrest/js/app.js
+++ forrest/js/app.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
         designers = {
             forrest: 'codycobb',
             cam: 'ashthorp',
-            // ant: ''
+            ant: 'Filiphds'
         };
 
     //*******************//
@@ -27,11 +27,49 @@ $(document).ready(function() {
         });
     }
 
-    for (designer in designers) {
-        getData('https://api.behance.net/v2/users/' + designers[designer]+ '/projects?client_id=' + apiKey, function(data) {
-            console.log(data);
-        });
+    function getDesigners() {
+        for (var designer in designers) {
+            getData('https://api.behance.net/v2/users/' + designers[designer] + '?client_id=' + apiKey, function(data) {
+                console.log(data);
+
+                var user = data.user;
+
+                var designerTemplate = $('#designerName').html();
+                var compiledDesignerTemplate = Template7.compile(designerTemplate);
+                var designerInfo = {
+                    uid: user.username,
+                    name: user.display_name,
+                    job: user.occupation,
+                    designerImage: user.images[100],
+                    behance: user.url
+                };
+                var designers = compiledDesignerTemplate(designerInfo);
+
+                $('body').append(designers);
+
+                getData('https://api.behance.net/v2/users/' + user.username + '/projects?client_id=' + apiKey, function(projectResponse) {
+                    console.log(projectResponse);
+
+                    var projects = projectResponse.projects;
+
+                    for (var i = 0; i < projects.length; i++) {
+                        var proj = projects[i];
+                        var projectTemplate = $('#designerProjects').html();
+                        var compiledProjectTemplate = Template7.compile(projectTemplate);
+                        var projectInfo = {
+                            coverImage: proj.covers.original,
+                            projectName: proj.name
+                        };
+                        var project = compiledProjectTemplate(projectInfo);
+
+                        $('#' + user.username).append(project);
+                    }
+                });
+            });
+        }
     }
+
+    getDesigners();
 
     //*************************//
     //**** Event Listeners ****//
