85cfd16addb158e9056505a4e75d608e91522c21
Forrest Wilson <forrest.wilson@me.com>
2018-03-09 15:24:27 +1300

    Wrote basic generic AJAX function

--- forrest/js/app.js
+++ forrest/js/app.js
@@ -1,3 +1,34 @@
 $(document).ready(function() {
 
+    //*******************************//
+    //**** Variable Declarations****//
+    //*******************************//
+
+    var apiKey = '8PyTZKuJ1VEQWvDvoOsARLkT8f6N71db',
+        xhr = null; // Needed in order to abort the current AJAX request should another one come along
+
+    //*******************//
+    //**** Functions ****//
+    //*******************//
+
+    function getData(url, successFunction, completionHandler) {
+        if (xhr !== null && xhr.readyState != 4) {
+            xhr.abort();
+        }
+
+        xhr = $.ajax({
+            method: 'GET',
+            dataType: 'json',
+            url: url,
+            success: successFunction,
+            error: function(err) {
+                throw new Error('Unhandled AJAX Error: ', err);
+            },
+            always: completionHandler
+        });
+    }
+
+    //*************************//
+    //**** Event Listeners ****//
+    //*************************//
 });
