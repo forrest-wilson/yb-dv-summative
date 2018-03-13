381368a829886153e77120625bfd88e5918a81ca
Cameron mitchell Shaw <845872712@bun2-206-14.local>
2018-03-13 15:33:29 +1300

    basic structure built

--- cam/js/app.js
+++ cam/js/app.js
@@ -5,15 +5,14 @@ var secondProfile  	= document.getElementById('profileDos');
 var thirdProfile 	= document.getElementById('profileTres');
 
 firstProfile.addEventListener('click', displayDetail, false);
-secondProfile.addEventListener('click', displayDetail, false);
-thirdProfile.addEventListener('click', displayDetail, false);
+secondProfile.addEventListener('click', displayDetailSecondProfile, false);
+thirdProfile.addEventListener('click', displayDetailThirdProfile, false);
 
 function displayDetail() {
-
 	firstProfile.classList.remove('col-lg-3');
 	firstProfile.classList.add('col-lg-12');
-	$(firstProfile).css({'pointer-events': 'none'});
-	$(firstProfile).css({'height': '500px'});
+	// $(firstProfile).css({'pointer-events': 'none'});
+	$(firstProfile).css({'height': '550px'});
 
 	secondProfile.classList.remove('col-lg-3');
 	secondProfile.classList.add('col-lg-12');
@@ -23,8 +22,34 @@ function displayDetail() {
 	thirdProfile.classList.add('col-lg-12');
 	$(thirdProfile).css({'height': '80px'});
 }
+function displayDetailSecondProfile() {
+	secondProfile.classList.remove('col-lg-3');
+	secondProfile.classList.add('col-lg-12');
+	// $(secondProfile).css({'pointer-events': 'none'});
+	$(secondProfile).css({'height': '550px'});
+	
+	firstProfile.classList.remove('col-lg-3');
+	firstProfile.classList.add('col-lg-12');
+	$(firstProfile).css({'height': '80px'});
 
+	thirdProfile.classList.remove('col-lg-3');
+	thirdProfile.classList.add('col-lg-12');
+	$(thirdProfile).css({'height': '80px'});
+}
+function displayDetailThirdProfile() {
+	thirdProfile.classList.remove('col-lg-3');
+	thirdProfile.classList.add('col-lg-12');
+	// $(thirdProfile).css({'pointer-events': 'none'});
+	$(thirdProfile).css({'height': '550px'});
+	
+	secondProfile.classList.remove('col-lg-3');
+	secondProfile.classList.add('col-lg-12');
+	$(secondProfile).css({'height': '80px'});
 
+	firstProfile.classList.remove('col-lg-3');
+	firstProfile.classList.add('col-lg-12');
+	$(firstProfile).css({'height': '80px'});
+}
 
 
 
