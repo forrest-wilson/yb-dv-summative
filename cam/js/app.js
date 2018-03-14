3b9c798824e3bcbb061f685c51b47c7ba691c4e3
Cameron mitchell Shaw <845872712@bun2-206-14.local>
2018-03-14 17:53:24 +1300

    image sizing

--- cam/js/app.js
+++ cam/js/app.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
 
 var apiKey = 'Iamkq0sRqOd6PqC7IEFwofD3YVs30ox9';
-var ashThorpUrl = 'https://api.behance.net/v2/users/ashthorp?client_id=Iamkq0sRqOd6PqC7IEFwofD3YVs30ox9';
-var codyCobbUrl = 'https://api.behance.net/v2/users/codycobb?client_id=Iamkq0sRqOd6PqC7IEFwofD3YVs30ox9';
-var filipHodasUrl = 'https://api.behance.net/v2/users/Filiphds?client_id=Iamkq0sRqOd6PqC7IEFwofD3YVs30ox9';
+var apiKey2 = 'KxiDWLIreraa9YM633j8JQy3mRmU6Smh';
+var ashThorpUrl = 'https://api.behance.net/v2/users/ashthorp?client_id=KxiDWLIreraa9YM633j8JQy3mRmU6Smh';
+var codyCobbUrl = 'https://api.behance.net/v2/users/codycobb?client_id=KxiDWLIreraa9YM633j8JQy3mRmU6Smh';
+var filipHodasUrl = 'https://api.behance.net/v2/users/Filiphds?client_id=KxiDWLIreraa9YM633j8JQy3mRmU6Smh';
 
 var firstProfile  	= document.getElementById('profileUno');
 var secondProfile  	= document.getElementById('profileDos');
@@ -17,61 +18,64 @@ function displayDetail() {
 	firstProfile.classList.remove('col-lg-3');
 	firstProfile.classList.remove('col-md-3');
 	firstProfile.classList.add('col-lg-12');
-	firstProfile.classList.add('col-md-7');
+	firstProfile.classList.add('col-md-9');
 	// $(firstProfile).css({'pointer-events': 'none'});
-	$(firstProfile).css({'height': '550px'});
+	$(firstProfile).css({'height': '600px'});
+	$('img').css({'width': '25%'});
 
 	secondProfile.classList.remove('col-lg-3');
 	secondProfile.classList.remove('col-md-3');
 	secondProfile.classList.add('col-lg-12');
-	secondProfile.classList.add('col-md-7');
-	$(secondProfile).css({'height': '80px'});
+	secondProfile.classList.add('col-md-9');
+	$(secondProfile).css({'height': '60px'});
 
 	thirdProfile.classList.remove('col-lg-3');
 	thirdProfile.classList.remove('col-md-3');
 	thirdProfile.classList.add('col-lg-12');
-	thirdProfile.classList.add('col-md-7');
-	$(thirdProfile).css({'height': '80px'});
+	thirdProfile.classList.add('col-md-9');
+	$(thirdProfile).css({'height': '60px'});
 }
 function displayDetailSecondProfile() {
 	secondProfile.classList.remove('col-lg-3');
 	secondProfile.classList.remove('col-md-3');
 	secondProfile.classList.add('col-lg-12');
-	secondProfile.classList.add('col-md-7');
+	secondProfile.classList.add('col-md-9');
 	// $(secondProfile).css({'pointer-events': 'none'});
-	$(secondProfile).css({'height': '550px'});
+	$(secondProfile).css({'height': '600px'});
+	$('img').css({'width': '25%'});
 	
 	firstProfile.classList.remove('col-lg-3');
 	firstProfile.classList.remove('col-md-3');
 	firstProfile.classList.add('col-lg-12');
-	firstProfile.classList.add('col-md-7');
-	$(firstProfile).css({'height': '80px'});
+	firstProfile.classList.add('col-md-9');
+	$(firstProfile).css({'height': '60px'});
 
 	thirdProfile.classList.remove('col-lg-3');
 	thirdProfile.classList.remove('col-md-3');
 	thirdProfile.classList.add('col-lg-12');
-	thirdProfile.classList.add('col-md-7');
-	$(thirdProfile).css({'height': '80px'});
+	thirdProfile.classList.add('col-md-9');
+	$(thirdProfile).css({'height': '60px'});
 }
 function displayDetailThirdProfile() {
 	thirdProfile.classList.remove('col-lg-3');
 	thirdProfile.classList.remove('col-md-3');
 	thirdProfile.classList.add('col-lg-12');
-	thirdProfile.classList.add('col-md-7');
+	thirdProfile.classList.add('col-md-9');
 	// $(thirdProfile).css({'pointer-events': 'none'});
-	$(thirdProfile).css({'height': '550px'});
+	$(thirdProfile).css({'height': '600px'});
+	$('img').css({'width': '25%'});
 	
 	secondProfile.classList.remove('col-lg-3');
 	secondProfile.classList.remove('col-md-3');
 	secondProfile.classList.add('col-lg-12');
-	secondProfile.classList.add('col-md-7');
-	$(secondProfile).css({'height': '80px'});
+	secondProfile.classList.add('col-md-9');
+	$(secondProfile).css({'height': '60px'});
 
 	firstProfile.classList.remove('col-lg-3');
 	firstProfile.classList.remove('col-md-3');
 	firstProfile.classList.add('col-lg-12');
-	firstProfile.classList.add('col-md-7');
-	$(firstProfile).css({'height': '80px'});
+	firstProfile.classList.add('col-md-9');
+	$(firstProfile).css({'height': '60px'});
 }
 
 
