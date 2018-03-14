12b875179ee02f710309f74fe6f8e8f9964a53d7
Cameron mitchell Shaw <845872712@bun2-206-14.local>
2018-03-14 15:54:56 +1300

    implemented Behance API main page

--- cam/js/app.js
+++ cam/js/app.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 
+var apiKey = 'Iamkq0sRqOd6PqC7IEFwofD3YVs30ox9';
+var ashThorpUrl = 'https://api.behance.net/v2/users/ashthorp?client_id=Iamkq0sRqOd6PqC7IEFwofD3YVs30ox9';
+var codyCobbUrl = 'https://api.behance.net/v2/users/codycobb?client_id=Iamkq0sRqOd6PqC7IEFwofD3YVs30ox9';
+var filipHodasUrl = 'https://api.behance.net/v2/users/Filiphds?client_id=Iamkq0sRqOd6PqC7IEFwofD3YVs30ox9';
+
 var firstProfile  	= document.getElementById('profileUno');
 var secondProfile  	= document.getElementById('profileDos');
 var thirdProfile 	= document.getElementById('profileTres');
@@ -50,6 +55,88 @@ function displayDetailThirdProfile() {
 	firstProfile.classList.add('col-lg-12');
 	$(firstProfile).css({'height': '80px'});
 }
+
+
+$.ajax ({
+	url: ashThorpUrl,
+	dataType: 'jsonp',
+	success: function(behanceData) {
+		if (behanceData) {
+			console.log(behanceData);
+			var profileImage = '';
+			profileImage += '<img src= "' + behanceData.user.images[276] + '">' + '<figcaption>' + behanceData.user.display_name + '</figcaption>';
+			$(secondProfile).append(profileImage);
+		}
+	},
+	error: function(error) {
+		console.log('error');
+	}
+});
+
+$.ajax ({
+	url: codyCobbUrl,
+	dataType: 'jsonp',
+	success: function(behanceData) {
+		if (behanceData) {
+			console.log(behanceData);
+			var profileImage = '';
+			profileImage += '<img src= "' + behanceData.user.images[276] + '">' + '<figcaption>' + behanceData.user.display_name + '</figcaption>';
+			$(firstProfile).append(profileImage);
+		}
+	},
+	error: function(error) {
+		console.log('error');
+	}
+});
+$.ajax ({
+	url: filipHodasUrl,
+	dataType: 'jsonp',
+	success: function(behanceData) {
+		if (behanceData) {
+			console.log(behanceData);
+			var profileImage = '';
+			profileImage += '<img src= "' + behanceData.user.images[276] + '">' + '<figcaption>' + behanceData.user.display_name + '</figcaption>';
+			$(thirdProfile).append(profileImage);
+		}
+	},
+	error: function(error) {
+		console.log('error');
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
