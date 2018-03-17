8a5d0f5fdfbc0ec1f55c936db24b09f409cc4825
ayersanth <ayersanth@gmail.com>
2018-03-17 14:10:24 +1300

    Connected flip animation to click event

--- anthony/js/app.js
+++ anthony/js/app.js
@@ -8,20 +8,27 @@ $(document).ready(function() {
 		statsToday = [];
 
 
-	// Function for grabbing data from behance
+	// Grabbing data from behance
 	function getData(url) {
         $.ajax({
             dataType: 'jsonp',
             url: url,
             success: function(behanceData) {
             	statsAll.push(behanceData);
-            	
         	},
             error: function(error) {
                console.log(error);
             }
         });
     }
+
+    // Container Flip
+    document.getElementById('graphContainer').addEventListener('click', function() {
+		var element = document.getElementById('graphContainer');
+		element.classList.toggle('flip');
+    });
+    
+
 
     // data grab test
     getData(usersURL + userIds[0] + '/stats?client_id=' + apiKey);
