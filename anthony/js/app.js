c57068536696b4c41bd37ece4f4e95c3a20be01b
ayersanth <ayersanth@gmail.com>
2018-03-18 17:45:54 +1300

    Google charts connected with API stats and CSS3 flip interaction

--- anthony/js/app.js
+++ anthony/js/app.js
@@ -1,7 +1,8 @@
-$(document).ready(function() {
-	google.charts.load('current', {'packages':['corechart', 'bar']});
-	// google.charts.setOnLoadCallback(drawChart);
+google.charts.load('current', {'packages':['corechart', 'bar']});
+google.charts.setOnLoadCallback(drawChart);
 
+function drawChart() {
+	
 	var apiKey = 'nnHT2gNBLBc7MnBAi0G0pGrwuFHmhXzi';
 		usersURL = 'https://api.behance.net/v2/users/',
 		projectsURL = 'https://www.behance.net/v2/projects/',
@@ -34,39 +35,94 @@ $(document).ready(function() {
 	// Pushes data to array
 	function populateData() {
 
-		// statsAll = [];
+		statsAll = [];
 
 		for (var i = 0; i < userIds.length; i++) {
 			dataGrab(usersURL + userIds[i] + '/stats?client_id=' + apiKey);
-		};
-            	
+		}
+        
+        setTimeout(function() {
+			var data = google.visualization.arrayToDataTable([
+				['Designers', 'Project Views', { role: 'style' }, 'Profile Views', { role: 'style' }, 'Project Appreciations', { role: 'style' }],
+				['Cody Cobb', statsAll[0].stats.all_time.project_views, '#eb0d30', statsAll[0].stats.all_time.profile_views, '#cff074', statsAll[0].stats.all_time.project_appreciations, '#a9e6ec'],
+				['Ash Thorp', statsAll[1].stats.all_time.project_views, '#eb0d30', statsAll[1].stats.all_time.profile_views, '#cff074', statsAll[1].stats.all_time.project_appreciations, '#a9e6ec'],
+				['Filip Hodas', statsAll[2].stats.all_time.project_views, '#eb0d30', statsAll[2].stats.all_time.profile_views, '#cff074', statsAll[2].stats.all_time.project_appreciations, '#a9e6ec']
+			]);
+			var options = {
+                height: 500,
+                width: 700,
+                legend: { position: 'top', maxLines: 3 },
+                colors: ['#eb0d30', '#cff074', '#a9e6ec'],
+                minorTicks: 10
+            };
+            var chart = new google.visualization.ColumnChart(document.getElementById('chartLocation'));
+            chart.draw(data, options);
+			console.log(statsAll[0].stats.all_time.project_views);
+		}, 500);
+          	
     }
 
-    // Draw bar chart
-	function drawChart() {
-        
-        populateData();
-        
-		var data = google.visualization.arrayToDataTable([
-		  ['Designers', 'Project Views', 'Profile Views', 'Project Appreciations', 'Project Comments'],
-		  ['Cody Cobb', statsAll[0].stats.all_time.project_views, statsAll[0].stats.all_time.project_views],
-		  ['Ash Thorp', behanceData.with[0].content.temp],
-		  ['Filip Hodas', behanceData.with[0].content.temp]
-		]);
+    // Remove elements
+	function removeElements(ele) {
+		var img = ele;
+	    while (img.hasChildNodes()) {   
+		    img.removeChild(img.firstChild);
+		}
+	};
 
-		console.log(statsAll[0].stats.all_time.project_views);
-               	
-    }
+    // Container Flips
+    document.getElementById('cody').addEventListener('click', function() {
 
-    
-	// console.log(statsAll[0].stats.all_time.project_views);
-    
+		var img = document.createElement('img');
+		img.src = 'img/line_graph.png';
+		document.getElementById('dataLocation').appendChild(img);
 
-    // Container Flip
-    document.getElementById('graphContainer').addEventListener('click', function() {
-		var element = document.getElementById('graphContainer');
-		element.classList.toggle('flip');
+		setTimeout(function() {
+			var element = document.getElementById('graphContainer');
+			element.classList.toggle('flip');
+		}, 100);
+
+    });
+
+    document.getElementById('ash').addEventListener('click', function() {
+
+		var img = document.createElement('img');
+		img.src = 'img/line_graph.png';
+		document.getElementById('dataLocation').appendChild(img);
+
+		setTimeout(function() {
+			var element = document.getElementById('graphContainer');
+			element.classList.toggle('flip');
+		}, 100);
+
+    });
+
+    document.getElementById('filip').addEventListener('click', function() {
+
+		var img = document.createElement('img');
+		img.src = 'img/line_graph.png';
+		document.getElementById('dataLocation').appendChild(img);
+
+		setTimeout(function() {
+			var element = document.getElementById('graphContainer');
+			element.classList.toggle('flip');
+		}, 100);
+
     });
     
+    document.getElementById('dataLocation').addEventListener('click', function() {
 
-});
+   		removeElements(document.getElementById('dataLocation'));
+
+   		var element = document.getElementById('graphContainer');
+		element.classList.toggle('flip');
+
+	});
+
+	populateData();
+
+	// Chart update
+	// setInterval(function() {
+	//     drawChart();
+	// }, 10000);
+}
